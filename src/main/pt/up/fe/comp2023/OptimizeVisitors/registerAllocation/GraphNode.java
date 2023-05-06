2badcda7e521c5940cbf169a29f41c55dd3ca7b1
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-05-06 14:59:08 +0100

    living the life, or has they say, liveness of vars

--- src/main/pt/up/fe/comp2023/OptimizeVisitors/registerAllocation/GraphNode.java
+++ src/main/pt/up/fe/comp2023/OptimizeVisitors/registerAllocation/GraphNode.java
@@ -1,10 +1,84 @@
 package pt.up.fe.comp2023.OptimizeVisitors.registerAllocation;
 
+import org.specs.comp.ollir.Instruction;
+import org.specs.comp.ollir.Node;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GraphNode {
-    private List<GraphNode> predecessors = new ArrayList<>();
-    private List<GraphNode> successors = new ArrayList<>();
+    private final List<GraphNode> predecessors = new ArrayList<>();
+    private final List<GraphNode> successors = new ArrayList<>();
 
+    private Instruction instruction;
+
+    private final Set<Integer> in = new HashSet<>();
+    private final Set<Integer> out = new HashSet<>();
+    private final Set<Integer> use = new HashSet<>();
+    private final Set<Integer> def = new HashSet<>();
+
+    public GraphNode(Instruction instruction) {
+        this.instruction = instruction;
+    }
+
+    public void addPredecessor(GraphNode predecessor) {
+        predecessors.add(predecessor);
+    }
+
+    public void addSuccessor(GraphNode successor) {
+        successors.add(successor);
+    }
+
+    public List<GraphNode> getPredecessors() {
+        return predecessors;
+    }
+
+    public List<GraphNode> getSuccessors() {
+        return successors;
+    }
+
+    public Instruction getInstruction() {
+        return instruction;
+    }
+
+    public Set<Integer> getIn() {
+        return in;
+    }
+
+    public Set<Integer> getOut() {
+        return out;
+    }
+
+    public Set<Integer> getUse() {
+        return use;
+    }
+
+    public Set<Integer> getDef() {
+        return def;
+    }
+
+    static List<GraphNode> getFromNodes(List<Instruction> instructions) {
+        List<GraphNode> nodes = new ArrayList<>();
+        for (Instruction instruction : instructions) {
+            nodes.add(new GraphNode(instruction));
+        }
+        return nodes;
+    }
+
+    @Override
+    public String toString() {
+        return instruction.toString();
+    }
+
+    public void setOut(Set<Integer> out) {
+        this.out.clear();
+        this.out.addAll(out);
+    }
+
+    public void setIn(Set<Integer> in) {
+        this.in.clear();
+        this.in.addAll(in);
+    }
 }
--- src/main/pt/up/fe/comp2023/OptimizeVisitors/registerAllocation/RegisterAllocation.java
+++ src/main/pt/up/fe/comp2023/OptimizeVisitors/registerAllocation/RegisterAllocation.java
@@ -1,6 +1,11 @@
 package pt.up.fe.comp2023.OptimizeVisitors.registerAllocation;
 
 import org.specs.comp.ollir.ClassUnit;
+import org.specs.comp.ollir.Node;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class RegisterAllocation {
 
@@ -14,7 +19,46 @@ public class RegisterAllocation {
 
     public void liveliness() {
 
+
         classUnit.getMethods().forEach(method -> {
+            // method.buildCFG(); .... cfg ??? perguntar ao professor o pk de este metodo existir
+            method.buildCFG();
+
+            List<GraphNode> nodes = GraphNode.getFromNodes(method.getInstructions());
+
+            boolean changed;
+            do {
+                changed = false;
+                for (GraphNode node : nodes) {
+
+                    Set<Integer> inL = node.getIn(); // in'
+                    Set<Integer> outL = node.getOut(); // out'
+
+
+                    Set<Integer> use = node.getUse();
+                    Set<Integer> def = node.getDef();
+
+                    // in[n] = use[n] U (out[n] - def[n])
+                    Set<Integer> diff = new HashSet<>(node.getOut());
+                    diff.removeAll(def);
+
+                    use.addAll(diff);
+
+                    node.setIn(use);
+
+                    Set<Integer> outAux = new HashSet<>();
+                    for (GraphNode successor : node.getSuccessors()) {
+                        outAux.addAll(successor.getIn());
+                    }
+
+                    node.setOut(outAux); // ver se é necessário
+
+                    if (!inL.equals(node.getIn()) || !outL.equals(node.getOut())) {
+                        changed = true;
+                    }
+
+                }
+            } while (!changed);
 
         });
 
